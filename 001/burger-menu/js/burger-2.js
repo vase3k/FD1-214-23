eeff23e748199afe8927cae7549b0dec56dae265
vasiliy <vase3k@gmail.com>
2023-07-03 23:42:47 +0300

    burger, sass

---
+++ 001/burger-menu/js/burger-2.js
@@ -0,0 +1,6 @@
+(function(){
+    const burger = document.querySelector('.burger');
+        burger.addEventListener('click', function () {
+            burger.classList.toggle('burger_active');
+        });
+})();
